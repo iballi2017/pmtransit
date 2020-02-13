deebf5dec91281fee91afe78658db0c12f70e821
iballi2017 <iballi2017@gmail.com>
2020-02-13 11:47:57 +0100

    13/02/2020 commit

---
+++ includes/header.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PM Transit</title>
+    <!-- font family -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,900&display=swap" rel="stylesheet">
+    <!-- Font awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0/css/all.min.css">
+    <!-- Bootstrap 4.4.1 -->
+    <link rel="stylesheet" href="../bootstrap-4.4.1-dist/css/bootstrap.min.css">
+    <!-- custom css -->
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light shadow sticky-top">
+
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <h2>Brand Logo</h2>
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fas fa-ellipsis-v fa-1x"></i>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item main-animate-nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item main-animate-nav-item">
+                        <a class="nav-link" href="#">About Us</a>
+                    </li>
+                    <li class="nav-item main-animate-nav-item">
+                        <a class="nav-link" href="#">Services</a>
+                    </li>
+                    <li class="nav-item main-animate-nav-item">
+                        <a class="nav-link" href="#">Contact Us</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="btn btn-outline-secondary btn-sm nav-link px-3 mx-2" href="#">Sign In</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="btn btn-outline-secondary btn-sm nav-link px-3 mx-2" href="#">Sign Up</a>
+                    </li>
+                    <li class="nav-item active">
+                        <buton class="btn btn-outline-secondary btn-sm nav-link px-3 mx-2">Logout</buton>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
